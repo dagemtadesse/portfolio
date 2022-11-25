8b051387ee826fcc6b11f2e0e425e15dec449353
dagem-kemer <dagem@kemercode.com>
2022-11-25 17:53:49 +0300

    bg image cover

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -37,7 +37,7 @@ export default function App({ Component, pageProps }: AppProps) {
         className="absolute bottom-0 top-0 left-0 right-0 bg-white z-10 shadow-xl overflow-hidden"
         style={props}
       >
-        <div className="overflow-hidden w-screen h-screen relative z-10 bg-blur bg-no-repeat"></div>
+        <div className="overflow-hidden w-screen h-screen relative z-10 bg-blur bg-no-repeat  bg-cover"></div>
 
         {/* body */}
         {currentPage === 'Portfolio' && (
